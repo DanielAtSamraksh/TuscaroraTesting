8d56a57d11cee783ddca9acbf3cfc4a763c2b0b2
Daniel Patru <daniel.patru@samraksh.com>
2015-05-07 21:54:15 -0400

    Add more repos to test.

--- cron.sh
+++ cron.sh
@@ -3,4 +3,4 @@ set -x
 # cron will not source .bashrc, so we need to do it in a script.
 HOME=/home/dan
 source ${HOME}/.bashrc
-${HOME}/TuscaroraTesting/testBranch.py ~/Tuscarora/TuscaroraFW/validate.sh dev master
+${HOME}/TuscaroraTesting/testBranch.py ~/Tuscarora/TuscaroraFW/validate.sh dev master v095 rcop
