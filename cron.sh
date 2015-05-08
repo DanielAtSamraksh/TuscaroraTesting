338d1ec0a931ef3c59d31714d7439e5cc770bee2
Daniel Patru <daniel.patru@samraksh.com>
2015-05-07 21:52:26 -0400

    Adjust pathname of testing script.

--- cron.sh
+++ cron.sh
@@ -3,4 +3,4 @@ set -x
 # cron will not source .bashrc, so we need to do it in a script.
 HOME=/home/dan
 source ${HOME}/.bashrc
-${HOME}/Tuscarora/Testing/testBranch.py ~/Tuscarora/TuscaroraFW/validate.sh dev master
+${HOME}/TuscaroraTesting/testBranch.py ~/Tuscarora/TuscaroraFW/validate.sh dev master
